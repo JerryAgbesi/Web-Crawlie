68b7ba6c683e63891e511eb54ce46224bd4dfdb9
JerryAgbesi <jerryeagbesi@gmail.com>
2022-10-29 22:42:53 +0000

    Add functions to get content of the response

--- src/app.py
+++ src/app.py
@@ -13,23 +13,38 @@ class URL(BaseModel):
 
 @app.get("/")
 def home():
-    return {"message":"let's go faster"}
+    return {"msg":"Welcome to web Crawlie"}
 
 @app.post("/scrape_url")
 async def scrape_url(url:URL):
 
+    #get url requested by the user
     page = requests.get(str(url.url))
 
     soup = BeautifulSoup(page.text,"html.parser")
 
+    #get website's title,decription,keywords and image
     def get_title():
-        return soup.head.find("title").text if "title" in soup.head else None
+        return soup.head.find("title").text if soup.head.find("title") else None
 
-    # def get_description():
+    def get_description():
+        main_desc = soup.head.find("meta",attrs={'name':'description'}).get("content") if soup.head.find("meta",attrs={'name':'description'}) else None 
+        
+        og_desc = soup.find("meta",property="og:description").get("content") if soup.find("meta",property="og:description") else None
 
+        return main_desc or og_desc
+
+    def get_keywords():
+        return soup.head.find("meta",attrs={'name':'keywords'}).get('content') if soup.head.find("meta",attrs={'name':'keywords'}) else None 
+
+    def get_image():
+        return soup.find("meta",property="og:image").get("content") if soup.find("meta",property="og:image") else None    
 
     return {
-        "title":get_title()
+        "title":get_title(),
+        "Description":get_description(),
+        "Keywords":get_keywords(),
+        "Image":get_image()
     }
 
 
